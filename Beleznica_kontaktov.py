8c50103c820cd16fb90c7a8d6750d112f18087d7
Klemen1975 <rokj@rasca.net>
2016-03-10 20:55:39 +0100

    popravek

--- Beleznica_kontaktov.py
+++ Beleznica_kontaktov.py
@@ -14,19 +14,16 @@ class kontakti:
         self.phone=phone
         self.address=adress
 
-    def polno_ime(self):
-        print ("%s %s" %(self.first_name, self.last_name))
-
     def vnos_kontakta():
-        return
 
         vnos=raw_input("Želite dodati nov kontakt(ja ali ne):")
+        seznam=[]
 
         if vnos ==  "ja":
             print "Vensite zahtevane podatke:"
 
             repeat = "da"
-            seznam=[]
+
 
             while repeat.lower().strip() == 'da':
 
@@ -36,11 +33,17 @@ class kontakti:
                 phone = raw_input("Vpiši številko:")
                 address = raw_input("Vpiši naslov:")
 
-                kontakt = (first_name+""+last_name+""+email+""+phone+""+address)
+                kontakt = (first_name+" "+last_name+", "+email+", "+phone+", "+address)
 
                 seznam.append(kontakt)
 
                 repeat=raw_input("Ali boste dodali še en kontakt: (da ali ne)? ")
+        else:
+            print ("Pa kdaj drugič :P")
+
+        for kontakt in seznam:
+            print (kontakt)
+
     vnos_kontakta()
 
 
